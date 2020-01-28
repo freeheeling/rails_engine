d1ecf9aedd5ec662cf84e3dcea88f9aa887522e3
Scott Regenthal <scottregenthal@gmail.com>
2020-01-28 14:23:40 -0700

    Add customer model spec and customer factory

---
+++ spec/factories/customers.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :customer do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    created_at { DateTime.now.strftime('%F %T UTC') }
+    updated_at { DateTime.now.strftime('%F %T UTC') }
+  end
+end
