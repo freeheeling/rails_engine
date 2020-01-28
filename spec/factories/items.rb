ce85cf7d21f67ebdb553091bba1b9bb68af447cf
Scott Regenthal <scottregenthal@gmail.com>
2020-01-28 15:40:03 -0700

    Add item model spec and item factory

---
+++ spec/factories/items.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :item do
+    name { Faker::Commerce.product_name }
+    description { Faker::Company.catch_phrase }
+    unit_price { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
+    merchant { nil }
+    created_at { DateTime.now.strftime('%F %T UTC') }
+    updated_at { DateTime.now.strftime('%F %T UTC') }
+  end
+end
