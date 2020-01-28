cb37d4ce8e0f25e6c7bcabf14654dc4433dd9fc7
Scott Regenthal <scottregenthal@gmail.com>
2020-01-28 15:28:20 -0700

    Add invoice model spec and invoice factory

---
+++ spec/factories/invoices.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :invoice do
+    customer { nil }
+    merchant { nil }
+    status { 'shipped' }
+    created_at { DateTime.now.strftime('%F %T UTC') }
+    updated_at { DateTime.now.strftime('%F %T UTC') }
+  end
+end
