882061f2b8eb25e55a2916501a178d4fd1d9c2b8
Scott Regenthal <scottregenthal@gmail.com>
2020-01-28 16:16:31 -0700

    Update merchant model and spec with additional through relationships

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  describe 'relationships' do
+    it { should have_many :invoices }
+    it { should have_many :items }
+    it { should have_many(:invoice_items).through(:invoices) }
+    it { should have_many(:customers).through(:invoices) }
+    it { should have_many(:transactions).throuh(:invoices) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of :name }
   end
