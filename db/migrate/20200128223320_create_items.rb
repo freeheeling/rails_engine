d0ce2e993ef3b659f6e7141d7c8ebf992ba1f763
Scott Regenthal <scottregenthal@gmail.com>
2020-01-28 15:39:17 -0700

    Create and migrate items table

---
+++ db/migrate/20200128223320_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    enable_extension 'citext'
+
+    create_table :items do |t|
+      t.citext :name
+      t.text :description
+      t.float :unit_price
+      t.references :merchant, foreign_key: true
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps
+    end
+  end
+end
