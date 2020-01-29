1847ddf4f314049fa35912ac4efab0bfb1fabb56
Scott Regenthal <scottregenthal@gmail.com>
2020-01-29 07:09:17 -0700

    Add merchant items relationship endpoint spec

---
+++ spec/api/v1/requests/merchants/relationship_endpts_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants API relationship endpoints' do
+  it 'gets a collection of items associated with a specific merchant' do
+    merchant = create(:merchant)
+    create_list(:item, 2, merchant: merchant)
+
+    get "/api/v1/merchants/#{merchant.id}/items"
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(parsed_items[:data].count).to eq(2)
+  end
+end
