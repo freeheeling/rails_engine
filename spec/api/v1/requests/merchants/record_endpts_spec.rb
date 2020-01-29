d3d33abf2e655fc68b1a52185baf2d9757f0cf28
Scott Regenthal <scottregenthal@gmail.com>
2020-01-28 21:34:04 -0700

    Add merchant index endpoint spec

---
+++ spec/api/v1/requests/merchants/record_endpts_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants API' do
+  it 'can get a list of merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(merchants[:data].count).to eq(3)
+    expect(merchants[:data].first[:attributes]).to include(:name)
+  end
+end
