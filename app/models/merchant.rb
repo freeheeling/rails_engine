29d235d6795181778f978fdba18e46fc43f1044f
Scott Regenthal <scottregenthal@gmail.com>
2020-01-30 16:05:24 -0700

    Add class method for ranking variable merchants by total revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,15 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :invoices
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
-  
+
   validates_presence_of :name
+
+  def self.revenue_ranked(quantity)
+    joins(:invoice_items, :transactions).
+      select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue').
+      group(:id).
+      where(transactions: {result: 'success'}).
+      order('revenue DESC').
+      limit(quantity)
+  end
 end
