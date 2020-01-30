aff5cabdf8aaf09f38ab89e8e3ca922766944350
Scott Regenthal <scottregenthal@gmail.com>
2020-01-30 10:55:41 -0700

    Create merchants query controller with show action for find endpoint

---
+++ app/controllers/api/v1/merchants/query_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::Merchants::QueryController < ApplicationController
+  def show
+    merchant = Merchant.find_by(query_params)
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  private
+    def query_params
+      params.permit(:id, :name, :created_at, :updated_at)
+    end
+end
