6d23351beab9d713d754018e76935b4d7984a3db
Scott Regenthal <scottregenthal@gmail.com>
2020-01-30 13:10:51 -0700

    Add find_all merchants endpoint to index action in query controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/:id/items', to: 'items#index'
         get '/find', to: 'query#show'
+        get '/find_all', to: 'query#index'
       end
       resources :merchants, only: [:index, :show]
     end
