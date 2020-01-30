182b457a079260ffc935a8774f683cffff9cff63
Scott Regenthal <scottregenthal@gmail.com>
2020-01-30 14:18:14 -0700

    Add invoice relationship endpoint test

--- spec/api/v1/requests/merchants/relationship_endpts_spec.rb
+++ spec/api/v1/requests/merchants/relationship_endpts_spec.rb
@@ -10,6 +10,23 @@ RSpec.describe 'Merchants API relationship endpoints' do
     items = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
+    expect(items).to be_instance_of Hash
     expect(items[:data].count).to eq(2)
+    expect(items[:data].first[:type]).to eq('item')
+  end
+
+  it 'gets a collection of invoices associated with a specific merchant' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    create_list(:invoice, 2, customer: customer, merchant: merchant)
+
+    get "/api/v1/merchants/#{merchant.id}/invoices"
+
+    invoices = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(invoices).to be_instance_of Hash
+    expect(invoices[:data].size).to eq(2)
+    expect(invoices[:data].first[:type]).to eq('invoice')
   end
 end
