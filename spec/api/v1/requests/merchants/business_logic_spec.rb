533c8f9f09e6dd3da97d0b2cafc77ff908f1dd0a
Scott Regenthal <scottregenthal@gmail.com>
2020-01-31 21:11:45 -0700

    Add test to get a merchant's favorite customer (most transactions)

--- spec/api/v1/requests/merchants/business_logic_spec.rb
+++ spec/api/v1/requests/merchants/business_logic_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Merchants API business intelligence endpoints' do
   before :each do
-    # customer_1 = create(:customer)
-    # customer_2 = create(:customer)
+    customer_1 = create(:customer)
+    @customer_2 = create(:customer)
     @merchant_1 = create(:merchant)
     @merchant_2 = create(:merchant)
     @merchant_3 = create(:merchant)
-    # merchant_4 = create(:merchant)
+    @merchant_4 = create(:merchant)
     @invoice_1 = create(:invoice, merchant: @merchant_1)
     invoice_2 = create(:invoice, merchant: @merchant_2)
     invoice_3 = create(:invoice, merchant: @merchant_3)
-    # invoice_4 = create(:invoice, merchant: merchant_4)
+    invoice_4 = create(:invoice, merchant: @merchant_4, customer: customer_1)
+    invoice_5 = create(:invoice, merchant: @merchant_4, customer: @customer_2)
+    invoice_6 = create(:invoice, merchant: @merchant_4, customer: @customer_2)
     create(:invoice_item, quantity: '3', unit_price: '1.00', invoice: @invoice_1)
     create(:invoice_item, quantity: '2', unit_price: '2.00', invoice: invoice_2)
     create(:invoice_item, quantity: '1', unit_price: '2.50', invoice: invoice_3)
@@ -19,6 +21,9 @@ RSpec.describe 'Merchants API business intelligence endpoints' do
     create(:transaction, invoice: @invoice_1)
     create(:transaction, invoice: invoice_2)
     create(:transaction, invoice: invoice_3)
+    create(:transaction, invoice: invoice_4)
+    create(:transaction, invoice: invoice_5)
+    create(:transaction, invoice: invoice_6)
     # create(:transaction, result: 'failed', invoice: invoice_4)
   end
 
@@ -42,5 +47,14 @@ RSpec.describe 'Merchants API business intelligence endpoints' do
 
     expect(response).to be_successful
     expect(revenue[:data][:attributes][:total_revenue].to_f).to eq(total_revenue)
+  end
+
+  it 'can get customer with most successful transactions for a merchant' do
+    get "/api/v1/merchants/#{@merchant_4.id}/favorite_customer"
+
+    customer = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(customer[:data][:id].to_i).to eq(@customer_2.id)
   end
 end
