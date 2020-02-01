41cbe828eb8b17097aaa3182aab94cf25ce5095f
Scott Regenthal <scottregenthal@gmail.com>
2020-01-31 17:57:39 -0700

    Add show action to merchants revenue controller utilizing new poro

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::Merchants::RevenueController < ApplicationController
     merchants = Merchant.revenue_ranked(params[:quantity])
     render json: MerchantSerializer.new(merchants)
   end
+
+  def show
+    revenue = Merchant.revenue_on_date(params[:date])
+    render json: MerchantRevenue.new(revenue).as_json
+  end
 end
