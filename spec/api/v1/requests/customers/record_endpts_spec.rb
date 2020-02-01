8769144493def2b92b4a4838a18013ec9ca6c91c
Scott Regenthal <scottregenthal@gmail.com>
2020-01-31 22:15:11 -0700

    Create customers record endpoint spec to test get list of all customers

---
+++ spec/api/v1/requests/customers/record_endpts_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Customers API record endpoints' do
+  it 'can get a list of all customers' do
+    create_list(:customer, 3)
+
+    get '/api/v1/customers'
+
+    customers = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(customers[:data].size).to eq(3)
+  end
+end
