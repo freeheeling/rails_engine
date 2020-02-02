70e0eddd583173d5dccad6d76b3ef199f91db080
Scott Regenthal <scottregenthal@gmail.com>
2020-02-02 07:18:15 -0700

    Add test to get merchant endpoint for given item

--- spec/api/v1/requests/items/relationship_endpts_spec.rb
+++ spec/api/v1/requests/items/relationship_endpts_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe 'Items API relationship endpoints' do
     expect(invoice_items[:data].size).to eq(2)
     expect(invoice_items[:data].first[:type]).to eq('invoice_item')
   end
+
+  it 'gets the merchant associated with a specific item' do
+    merchant = create(:merchant)
+    item = create(:item, merchant: merchant)
+
+    get "/api/v1/items/#{item.id}/merchant"
+
+    item_merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(item_merchant[:data][:id].to_i).to eq(merchant.id)
+  end
 end
