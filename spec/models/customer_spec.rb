7c83b5f23eb40d46ab11386ada60e9bf1b23e72c
Scott Regenthal <scottregenthal@gmail.com>
2020-02-01 20:53:39 -0700

    Add instance method test to customer model spec for favorite merchant

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -11,4 +11,27 @@ RSpec.describe Customer, type: :model do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
   end
+
+  describe 'instance methods' do
+    it 'favorite merchant' do
+      customer = create(:customer)
+      merchant_1 = create(:merchant)
+      merchant_1 = create(:merchant)
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+      merchant_2 = create(:merchant)
+      invoice_1 = create(:invoice, customer: customer, merchant: merchant_1)
+      invoice_2 = create(:invoice, customer: customer, merchant: merchant_1)
+      invoice_3 = create(:invoice, customer: customer, merchant: merchant_1)
+      invoice_4 = create(:invoice, customer: customer, merchant: merchant_2)
+      invoice_5 = create(:invoice, customer: customer, merchant: merchant_2)
+      create(:transaction, invoice: invoice_1)
+      create(:transaction, result: 'failed', invoice: invoice_2)
+      create(:transaction, result: 'failed', invoice: invoice_3)
+      create(:transaction, invoice: invoice_4)
+      create(:transaction, invoice: invoice_5)
+
+      expect(customer.favorite_merchant).to eq(merchant_2)
+    end
+  end
 end
