83afc35ca198cbdedf01b8222e7b8de8b71a2f5a
Scott Regenthal <scottregenthal@gmail.com>
2020-02-01 20:11:33 -0700

    Create invoices controller with index action utilizing serializer

---
+++ app/controllers/api/v1/customers/invoices_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Customers::InvoicesController < ApplicationController
+  def index
+    customer = Customer.find_by(id: params[:id])
+    render json: InvoiceSerializer.new(customer.invoices)
+  end
+end
