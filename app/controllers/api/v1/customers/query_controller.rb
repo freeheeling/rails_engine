5038379e4f4d4a60ec4264dc27f3ff12629b16d9
Scott Regenthal <scottregenthal@gmail.com>
2020-02-01 19:13:29 -0700

    Add index action to render serialized  attributes for matching query

--- app/controllers/api/v1/customers/query_controller.rb
+++ app/controllers/api/v1/customers/query_controller.rb
@@ -4,6 +4,11 @@ class Api::V1::Customers::QueryController < ApplicationController
     render json: CustomerSerializer.new(customer)
   end
 
+  def index
+    customers = Customer.where(query_params)
+    render json: CustomerSerializer.new(customers)
+  end
+
   private
     def query_params
       params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
