50e9e9c3128a00d3bfabd1b88e7644183729c767
Scott Regenthal <scottregenthal@gmail.com>
2020-02-01 20:55:37 -0700

    Add favorite_merchant instance method to customer model using AR query

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,17 @@
 class Customer < ApplicationRecord
   has_many :invoices
   has_many :invoice_items, through: :invoices
+  has_many :merchants, through: :invoices
   has_many :transactions, through: :invoices
 
   validates_presence_of :first_name, :last_name
+
+  def favorite_merchant
+    merchants.joins(:transactions).
+      select('merchants.*, count(invoices.merchant_id) AS sales').
+      merge(Transaction.successful).
+      group(:id).
+      order('sales DESC').
+      first
+  end
 end
