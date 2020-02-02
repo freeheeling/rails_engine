a31b309562d422c8d67f027747929d85898225a9
Scott Regenthal <scottregenthal@gmail.com>
2020-02-02 16:53:53 -0700

    Create model instance method test for specific item's best day

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,23 +16,44 @@ RSpec.describe Item, type: :model do
   end
 
   describe 'class methods' do
-    before :each do
+    it 'revenue_ranked' do
       item_1 = create(:item)
-      @item_2 = create(:item)
-      @item_3 = create(:item)
+      item_2 = create(:item)
+      item_3 = create(:item)
       invoice_1 = create(:invoice)
       invoice_2 = create(:invoice)
       invoice_3 = create(:invoice)
       create(:invoice_item, quantity: '1', unit_price: '3.00', item: item_1, invoice: invoice_1)
-      create(:invoice_item, quantity: '2', unit_price: '2.00', item: @item_2, invoice: invoice_2)
-      create(:invoice_item, quantity: '1', unit_price: '1.50', item: @item_3, invoice: invoice_3)
+      create(:invoice_item, quantity: '2', unit_price: '2.00', item: item_2, invoice: invoice_2)
+      create(:invoice_item, quantity: '1', unit_price: '1.50', item: item_3, invoice: invoice_3)
       create(:transaction, result: 'failed', invoice: invoice_1)
       create(:transaction, invoice: invoice_2)
       create(:transaction, invoice: invoice_3)
-    end
 
-    it 'revenue_ranked' do
-      expect(Item.revenue_ranked(2)).to eq([@item_2, @item_3])
+      expect(Item.revenue_ranked(2)).to eq([item_2, item_3])
+    end
+  end
+
+  describe 'instance methods' do
+    xit 'best_day' do
+      item = create(:item)
+      invoice_1 = create(:invoice, created_at: '2014-03-27')
+      invoice_2 = create(:invoice, created_at: '2014-03-27')
+      invoice_3 = create(:invoice, created_at: '2014-04-27')
+      invoice_4 = create(:invoice, created_at: '2014-04-27')
+      invoice_5 = create(:invoice, created_at: '2014-05-27')
+      create(:invoice_item, item: item, invoice: invoice_1)
+      create(:invoice_item, item: item, invoice: invoice_2)
+      create(:invoice_item, item: item, invoice: invoice_3)
+      create(:invoice_item, item: item, invoice: invoice_4)
+      create(:invoice_item, item: item, invoice: invoice_5)
+      create(:transaction, invoice: invoice_1)
+      create(:transaction, result: 'failed', invoice: invoice_2)
+      create(:transaction, invoice: invoice_3)
+      create(:transaction, result: 'failed', invoice: invoice_4)
+      create(:transaction, invoice: invoice_5)
+
+      expect(item.best_day(item.id)).to eq(invoice_5.created_at)
     end
   end
 end
