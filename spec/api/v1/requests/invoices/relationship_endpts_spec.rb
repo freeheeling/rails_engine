c0e911e41212523990bc75b73c6cdbb1a1cf23cd
Scott Regenthal <scottregenthal@gmail.com>
2020-02-02 20:02:58 -0700

    Add test get all invoice items associated with given invoice

--- spec/api/v1/requests/invoices/relationship_endpts_spec.rb
+++ spec/api/v1/requests/invoices/relationship_endpts_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe 'Invoices API realationship endpoints' do
     expect(transactions[:data].size).to eq(2)
     expect(transactions[:data].first[:type]).to eq('transaction')
   end
+
+  it 'gets a collection of invoice items associated with a given invoice' do
+    invoice = create(:invoice)
+    create_list(:invoice_item, 2, invoice: invoice)
+
+    get "/api/v1/invoices/#{invoice.id}/invoice_items"
+
+    invoice_items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(invoice_items).to be_instance_of Hash
+    expect(invoice_items[:data].size).to eq(2)
+    expect(invoice_items[:data].first[:type]).to eq('invoice_item')
+  end
 end
